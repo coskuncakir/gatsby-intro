e93c40991fd7bccca8ced235bd5f794b59b53c4d
coskun cakir <csknckr@gmail.com>
2020-04-30 15:37:28 +0100

    initial

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+function About() {
+  return (
+    <>
+      <h1>About me</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+        suscipit deleniti labore pariatur vero! Doloremque, placeat fugiat alias
+        laudantium exercitationem aspernatur mollitia, similique magnam,
+        voluptatem odio est possimus libero in!
+      </p>
+      <Link to="/">&larr; back to home</Link>
+    </>
+  );
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit
+        temporibus iusto distinctio repudiandae veniam ut deserunt eos sint
+        molestias, qui, fuga ex perferendis similique magnam est, nulla
+        architecto beatae laboriosam?
+      </p>
+      <Link to="/about">learn about me &rarr;</Link>
+    </>
+  );
+}
+
+export default Home;
