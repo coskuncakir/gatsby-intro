65351e34a37e2eb4e840b7e9e956dd8f381dc217
coskun cakir <csknckr@gmail.com>
2020-05-02 17:55:20 +0100

    optimized sharp images feature implemented

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 const Home = () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>
-        Hello{' '}
-        <span role="img" aria-label="wave">
-          👋
-        </span>{' '}
-        <Link to="/about">learn about me &rarr;</Link>
-      </p>
-      <hr />
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
 
